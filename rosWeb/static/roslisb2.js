11c5ce58cabd53a97035acd1207699da86665ef7
glace158 <jin7134@naver.com>
2023-05-25 17:03:42 +0900

    modify roslisb

--- rosWeb/static/roslisb2.js
+++ rosWeb/static/roslisb2.js
@@ -1,78 +1,126 @@
 // Ros WEB SOCKET SERVER CONF
 
 var ros = new ROSLIB.Ros({
-    url : 'ws:192.168.67.94:9090'
-  });
+  url : 'ws:192.168.67.94:9090'
+});
 
-  ros.on('connection', function() {
-    document.getElementById("status").innerHTML = "Connected";
-  });
+ros.on('connection', function() {
+  document.getElementById("status").innerHTML = "Connected";
+});
 
-  ros.on('error', function(error) {
-    document.getElementById("status").innerHTML = "Error";
-  });
+ros.on('error', function(error) {
+  document.getElementById("status").innerHTML = "Error";
+});
 
-  ros.on('close', function() {
-    document.getElementById("status").innerHTML = "Closed";
-  });
+ros.on('close', function() {
+  document.getElementById("status").innerHTML = "Closed";
+});
 
 function mapLoad(){
-  var viewer2D = new ROS2D.Viewer({
-    divID : 'map',
-    width : 700,
-    height : 700
-  });
+var viewer2D = new ROS2D.Viewer({
+  divID : 'map',
+  width : 700,
+  height : 700
+});
 
-  var gridClient = new ROS2D.OccupancyGridClient({
-    ros : ros,
-    rootObject : viewer2D.scene,
-    image: 'turtlebot.png',
-    continuous: true
-  });
+var gridClient = new ROS2D.OccupancyGridClient({
+  ros : ros,
+  rootObject : viewer2D.scene,
+  image: 'turtlebot.png',
+  continuous: true
+});
 
-  var robotMarker = new ROS2D.ArrowShape({
-    size : 1,
-    strokeSize : 1,
-    pulse : false
-  });
+var robotMarker = new ROS2D.ArrowShape({
+  size : 0.2,
+  strokeSize : 0.2,
+  pulse : false
+});
+/*
+var poseTopic = new ROSLIB.Topic({
+  ros : ros,
+  name : '/slam_out_pose',
+  messageType:'geometry_msgs/PoseStamped',
+});
+*/
 
-  var poseTopic = new ROSLIB.Topic({
-    ros : ros,
-    name : '/slam_out_pose',
-    messageType:'geometry_msgs/PoseStamped',
-  });
+var poseTopic = new ROSLIB.Topic({
+  ros : ros,
+  name : '/tf',
+  messageType: '/tf2_msgs/TFMessage',
+  //messageType:'tf/tfMessage'
+});
 
-  poseTopic.subscribe(
-    function (pose) {
-      console.log(pose);
-      console.log(pose.pose.position.x);
-    }
-  );
+/*
+poseTopic.subscribe(
+function(pose) {
+  robotMarker.x = pose.pose.position.x;
+  robotMarker.y = -pose.pose.position.y;
 
-  poseTopic.subscribe(
+  let orientationQuerter=pose.pose.orientation
+  var q0 = orientationQuerter.w;
+  var q1 = orientationQuerter.x;
+  var q2 = orientationQuerter.y;
+  var q3 = orientationQuerter.z;
+  degree=-Math.atan2(2 * (q0 * q3 + q1 * q2), 1 - 2 * (q2 * q2 + q3 * q3)) * 180.0 / Math.PI
+  robotMarker.rotation = degree;
+});
+*/
+let degree = 0;
+let posx = 0;
+let posy = 0;
+poseTopic.subscribe(
   function(pose) {
-    robotMarker.x = pose.pose.position.x;
-    robotMarker.y = -pose.pose.position.y;
-
-    let orientationQuerter=pose.pose.orientation
+    if (pose.transforms[0].child_frame_id != "base_footprint"){return;}
+    posx = pose.transforms[0].transform.translation.x;
+    posy = -pose.transforms[0].transform.translation.y;
+    robotMarker.x = posx;
+    robotMarker.y = -posy;
+    
+    let orientationQuerter=pose.transforms[0].transform.rotation;
     var q0 = orientationQuerter.w;
     var q1 = orientationQuerter.x;
     var q2 = orientationQuerter.y;
     var q3 = orientationQuerter.z;
     degree=-Math.atan2(2 * (q0 * q3 + q1 * q2), 1 - 2 * (q2 * q2 + q3 * q3)) * 180.0 / Math.PI
     robotMarker.rotation = degree;
-  });
+});
 
-  gridClient.rootObject.addChild(robotMarker);
+//rescuer marker
+var rescuerMarker = new ROS2D.ArrowShape({
+  size : 0.2,
+  strokeSize : 0.2,
+  pulse : false,
+  fillColor: createjs.Graphics.getRGB(255,0,0, 0.9)
+});
 
-  gridClient.on('change', function(){
-    viewer2D.scaleToDimensions(gridClient.currentGrid.width, gridClient.currentGrid.height);
-    viewer2D.shift(gridClient.currentGrid.pose.position.x, gridClient.currentGrid.pose.position.y);
-  });
+var distanceTopic = new ROSLIB.Topic({
+  ros : ros,
+  name : '/distance',
+  messageType: '/tf2_msgs/Int32',
+  //messageType:'tf/tfMessage'
+});
+
+distanceTopic.subscribe(
+  function (distance) {
+  let x = Math.sin(degree) * distance;
+  let y = Math.sqrt(Math.pow(distance) - Math.pow(x));
+  
+  rescuerMarker.x = posx + x;
+  rescuerMarker.y = posy + y;
+  rescuerMarker.rotation = 90;
+});
+
+gridClient.rootObject.addChild(robotMarker);
+
+resize = 0.5
+gridClient.on('change', function(){
+  viewer2D.scaleToDimensions(gridClient.currentGrid.width * resize, gridClient.currentGrid.height * resize);
+  viewer2D.shift(gridClient.currentGrid.pose.position.x * resize, gridClient.currentGrid.pose.position.y * resize);
+});
 }
 
 // window.onload는 최종에 있는거 한번만 실행됨
 window.addEventListener('onload', 
-  console.log("mapload"),
-  mapLoad()
+console.log("mapload"),
+mapLoad()
 )
